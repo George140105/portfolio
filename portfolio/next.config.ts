800bc64a38f275a05a8d2ccecc1eccef47739123
George Amgad <gogoamgad2005@gmail.com>
2025-05-25 16:09:21 +0300

    fix gihub pages

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // enables static export
+  // optional: if deploying to GitHub Pages
+  basePath: "/portfolio",
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
